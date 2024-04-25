27e390aa5f1692bbcf7960f14dedc487e5dc2ca7
alejandro8020 <alejandro_8020@hotmail.com>
2024-04-25 19:24:08 -0300

    menu lateral

--- formularios/form_maestro_design.py
+++ formularios/form_maestro_design.py
@@ -9,10 +9,11 @@ class FormularioMaestroDesign(tk.Tk):
     def __init__(self):
         super().__init__()
         self.logo = util_img.leer_imagen("./imagenes/logo_sameep.png",(560,136))
-        self.perfil = util_img.leer_imagen("./imagenes/sameep_log.webp",(100,100))
+        self.perfil = util_img.leer_imagen("./imagenes/logo_sameep.png",(150,100))
         self.config_window()
         self.paneles()
         self.controles_barra_superior()
+        self.controles_menu_lateral()
 
     def config_window(self):
         #Configuracion inicial de ventana
@@ -20,6 +21,7 @@ class FormularioMaestroDesign(tk.Tk):
         self.iconbitmap("./imagenes/logo_sameep.ico")
         w, h = 1024,660
         util_ventana.centrar_ventana(self,w,h)
+        
 
     def paneles(self):
         self.barra_superior = tk.Frame(
@@ -51,4 +53,47 @@ class FormularioMaestroDesign(tk.Tk):
         self.labelTitulo.config(fg="#fff", font=("Roboto",10), bg=COLOR_BARRA_SUPERIOR, padx=10, width=20)
         self.labelTitulo.pack(side=tk.RIGHT)
 
+    def controles_menu_lateral(self):
+        #Configuracion del menu lateral
+        ancho_menu = 20
+        alto_menu = 2
+        font_awesome =  font.Font(family='FontAwesome', size=15)
+        #Etiqueta de perfil
+        self.labelPerfil = tk.Label(self.menu_lateral, image=self.perfil, bg=COLOR_MENU_LATERAL)
+        self.labelPerfil.pack(side=tk.TOP, pady=10)
+
+        #Boton del menu lateral
+        self.buttonDashboard = tk.Button(self.menu_lateral)
+        self.buttonProfile = tk.Button(self.menu_lateral)
+        self.buttonPicture = tk.Button(self.menu_lateral)
+        self.buttonInfo = tk.Button(self.menu_lateral)
+        self.buttonSettings = tk.Button(self.menu_lateral)
+
+        button_info = [
+            ("Dashboard", '\uf109', self.buttonDashboard),
+            ("Profile", "\uf006", self.buttonProfile),
+            ("Picture", "\uf03e", self.buttonPicture),
+            ("Info", "\uf129",self.buttonInfo),
+            ("Settings", "\uf013",self.buttonSettings)
+        ]
+        for text, icon, button in button_info:
+            self.configurar_boton_menu(button, text, icon, font_awesome, ancho_menu, alto_menu)
+
+    def configurar_boton_menu(self, button, text, icon, font_awesome, ancho_menu, alto_menu):
+        button.config(text=f"{icon}  {text}", anchor="w", font=font_awesome,
+                     bd=0, bg=COLOR_MENU_LATERAL, fg="white", width=ancho_menu, height=alto_menu)
+        button.pack(side=tk.TOP)
+        self.bind_hover_events(button)
+
+    def bind_hover_events(self, button):
+        #Asociar eventos enter y leave con la funcion.
+        button.bind("<Enter>", lambda event: self.on_enter(event, button))
+        button.bind("<Leave>", lambda event: self.on_leave(event, button))
+
+    def on_enter(self,event,button):
+        #Cambia el estilo al pasar el raton por encima
+        button.config(bg=COLOR_MENU_CURSOR_ENCIMA, fg='white')
+    def on_leave(self, event, button):
+        #Restaurar estilo al salir el raton
+        button.config(bg=COLOR_MENU_LATERAL, fg='white')
 
